f497107f19b4ef07abe1c48788c5e421bb64725a
YPJ <244771293@qq.com>
2017-03-28 09:57:25 +0800

    初始化项目

---
+++ VideoShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoShow
+//
+//  Created by iKing on 2017/3/28.
+//  Copyright © 2017年 iKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
